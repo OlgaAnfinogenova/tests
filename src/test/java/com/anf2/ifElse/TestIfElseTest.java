661dd2e63120dffc9d57e8b720f7323eb9bb447e
anfi.olga <anfi.olga@gmail.com>
2023-07-23 12:38:11 +0100

    TEST_1: First simple task completed

---
+++ src/test/java/com/anf2/ifElse/TestIfElseTest.java
@@ -0,0 +1,20 @@
+package com.anf2.ifElse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TestIfElseTest {
+    TestIfElse testIfElse = new TestIfElse();
+
+    @Test
+    void testIfElse_inputAuto_Yes() {
+        String input = "Auto";
+        String expected = "Yes";
+        Assertions.assertEquals(expected, testIfElse.testIfElse(input));
+    }
+    @Test
+    void testIfElse_inputBmv_No() {
+        String input = "Auto";
+        String expected = "No";
+        Assertions.assertEquals(expected, testIfElse.testIfElse(input));
+    }
+}
