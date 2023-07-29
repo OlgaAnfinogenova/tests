1030df72db68fc5b67df584a57e4a0ad18f3eed4
olga <anfi.olga@gmail.com>
2023-07-29 09:19:49 -0400

    TEST_2: Second simple task completed

---
+++ src/main/java/com/anf2/ifElse/TestIfElse2.java
@@ -0,0 +1,16 @@
+package com.anf2.ifElse;
+
+/**
+ * Function testIfElse must return:
+ * -int 1 if first character in String(input) is 'A'
+ * -int 2 if first character in String(input) is 'A'
+ * -int 3 in another cases
+ * You should use if-else construction and method charAt() https://proglang.su/java/strings-charat
+ */
+public class TestIfElse2 {
+    public Integer testIfElse(String input) {
+        //Write your code here
+
+        return 0; //change 0 to variable which you want to return
+    }
+}
