a747e902a8095ffb7c6566f8c6aab1785efab0e2
olga <anfi.olga@gmail.com>
2023-07-29 09:08:16 -0400

    TEST_1: First simple task completed

---
+++ src/main/java/com/anf2/ifElse/TestIfElse.java
@@ -0,0 +1,14 @@
+package com.anf2.ifElse;
+
+/**
+ * Function testIfElse must return String "Yes" if first character in String(input) is "A" and return "No" if it is not
+ * You should use if-else construction and method charAt() https://proglang.su/java/strings-charat
+ */
+public class TestIfElse
+{
+    public String testIfElse( String input ) {
+        //Write your code here
+
+        return null; //change null to variable which you want to return
+    }
+}
