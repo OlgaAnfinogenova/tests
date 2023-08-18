c9018a832ab4f630a8fe993ea368854b6a95ae6c
anfi.olga <anfi.olga@gmail.com>
2023-08-18 19:17:35 +0100

    commit Solved

--- src/main/java/com/anf2/ifElse/TestIfElse2.java
+++ src/main/java/com/anf2/ifElse/TestIfElse2.java
@@ -3,14 +3,20 @@ package com.anf2.ifElse;
 /**
  * Function testIfElse must return:
  * -int 1 if first character in String(input) is 'A'
- * -int 2 if first character in String(input) is 'A'
+ * -int 2 if first character in String(input) is 'B'
  * -int 3 in another cases
  * You should use if-else construction and method charAt() https://proglang.su/java/strings-charat
  */
 public class TestIfElse2 {
     public Integer testIfElse(String input) {
-        //Write your code here
+        char result = input.charAt(0);
 
-        return 0; //change 0 to variable which you want to return
+        if (result == 'A') {
+            return 1;
+        } else if (result == 'B') {
+            return 2;
+        }
+
+        return 3; //change 0 to variable which you want to return
     }
 }
